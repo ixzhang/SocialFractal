ca42408d127a88ef52f8e537c7c9a74f70653f82
ixzhang <yxuan_zhang@outlook.com>
2025-02-17 10:28:26 +0800

    add: raw data

---
+++ R/raw_data.R
@@ -0,0 +1,15 @@
+#' Example Dataset: An Example of Raw Data
+#'
+#' This dataset contains spatial information of zebra finches in Replicate 1
+#'
+#' @format A list containing:
+#' \describe{
+#'   \item{id}{Individual ID}
+#'   \item{time}{the time points}
+#'   \item{space}{the number of perches where bird locations are captured}
+#'   \item{x}{the x-coordinate of location information}
+#'   \item{y}{the y-coordinate of location information}
+#'   \item{Replicate}{Replicate 1-4}
+#' }
+#' @source Generated from XYZ
+"raw_data"
